ac1bce7ef2a8c977302809f582f7dfb8f08b5518
hellosept1 <hellosept1@gmail.com>
2025-08-17 10:40:26 +0530

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+```js
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0f172a',
+        accent: '#D6F2B8'
+      }
+    }
+  },
+  plugins: [],
+}
+```
