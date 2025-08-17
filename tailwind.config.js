b9118f7033517282287062d95d1ac9e7d0044c93
hellosept1 <hellosept1@gmail.com>
2025-08-17 11:32:42 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
-```js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#0f172a',
-        accent: '#D6F2B8'
-      }
-    }
+    extend: {},
   },
   plugins: [],
 }
-```
